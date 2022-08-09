852cd8a70d1ab5f32b5da012e752f6e0a5d20ae4
Bráulio Bezerra <brauliobezerra@gmail.com>
2022-08-09 17:42:33 -0300

    very basic renderer

--- index.tsx
+++ index.tsx
@@ -21,3 +21,36 @@ const App = () => (
         <em>World</em>
     </div>
 );
+
+const render = (reactElementOrStringOrNumber, container) => {
+    if (['string', 'number'].includes(typeof reactElementOrStringOrNumber)) {
+        const textElement = document.createTextNode(String(reactElementOrStringOrNumber))
+        container.appendChild(textElement);
+        return;
+    }
+
+    const reactElement = reactElementOrStringOrNumber;
+
+    const actualDomElement = document.createElement(reactElement.tag);
+    if (reactElement.props) {
+        Object
+            .keys(reactElement.props)
+            .filter(p => p !== 'children')
+            .forEach(p => {
+                if (p === "class") {
+                    actualDomElement.className = reactElement.props[p];
+                } else {
+                    actualDomElement[p] = reactElement.props[p];
+                }
+
+            });
+
+        if (reactElement.props.children) {
+            reactElement.props.children.forEach(c => render(c, actualDomElement));
+        }
+    }
+
+    container.appendChild(actualDomElement);
+}
+
+render(<App />, document.querySelector("#app"));
