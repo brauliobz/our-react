f48ad86a0983d791773f23224fa9d47bf0bc98b4
Bráulio Bezerra <brauliobezerra@gmail.com>
2022-08-09 18:11:44 -0300

    basic state

--- index.tsx
+++ index.tsx
@@ -1,26 +1,46 @@
 let React = {
-    createElement: (tag_or_component, props, ...children) => {
-        if (typeof tag_or_component === 'function') {
-            return tag_or_component(props);
+    createElement: (tagOrComponent, props, ...children) => {
+        if (typeof tagOrComponent === 'function') {
+            return tagOrComponent(props);
         }
-        const tag = tag_or_component;
+        const tag = tagOrComponent;
         const element = {
             tag,
             props: { ...props, children }
         };
 
-        console.log(element);
-
         return element;
     }
 };
 
-const App = () => (
-    <div id="test" class="test_class">
-        Hello
-        <em>World</em>
-    </div>
-);
+const states = []
+let stateCursor = 0;
+
+const useState = (initialState) => {
+    const FROZEN_CURSOR = stateCursor;
+    states[FROZEN_CURSOR] = states[FROZEN_CURSOR] || initialState;
+    let setState = (newState) => {
+        states[FROZEN_CURSOR] = newState;
+        rerender();
+    };
+
+    stateCursor++;
+
+    return [states[FROZEN_CURSOR], setState]
+}
+
+const App = () => {
+    const [name, setName] = useState("person");
+    const [count, setCount] = useState(0);
+    return (
+        <div id="test" class="test_class">
+            <p>Hello <em>{name}</em></p>
+            <p>The count is {count}</p>
+            <button onclick={() => setCount(count + 1)}>+</button>
+            <input type="text" value={name} onchange={e => setName(e.target.value)}></input>
+        </div>
+    );
+};
 
 const render = (reactElementOrStringOrNumber, container) => {
     if (['string', 'number'].includes(typeof reactElementOrStringOrNumber)) {
@@ -51,6 +71,12 @@ const render = (reactElementOrStringOrNumber, container) => {
     }
 
     container.appendChild(actualDomElement);
+}
+
+const rerender = () => {
+    stateCursor = 0;
+    document.querySelector("#app").firstChild.remove()
+    render(<App />, document.querySelector("#app"));
 }
 
 render(<App />, document.querySelector("#app"));
